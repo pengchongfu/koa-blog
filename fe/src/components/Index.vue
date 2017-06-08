692ddbfa6d61a8b007a17d0a6109406c3735a7f4
pengchongfu <pengchongfu@gmail.com>
2017-06-08 19:24:33 +0800

    add marked

--- fe/src/components/Index.vue
+++ fe/src/components/Index.vue
@@ -1,17 +1,42 @@
 <template>
-  <div>
-    <p>{{ list }}</p>
-  </div>
+  <v-layout row wrap>
+    <v-flex xs8 offset-xs2>
+      <v-card v-for="item in list" class="card">
+        <v-card-row class="blue darken-1">
+          <v-card-title>
+            <span class="white--text">{{ item.title }}</span>   
+          </v-card-title>
+        </v-card-row>
+        <v-card-text>
+          <v-card-row height="75px">
+            <div>
+              <div>{{ item.user }}</div><div v-html="marked(item.content.slice(0, 30))"></div>
+            </div>
+          </v-card-row>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-row actions>
+          <v-btn flat class="green--text darken-1">查看</v-btn>
+        </v-card-row>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import axios from 'axios'
+import marked from 'marked'
 
 export default {
   name: 'Index',
   data () {
     return {
       list: []
+    }
+  },
+  methods: {
+    marked (value) {
+      return marked(value)
     }
   },
   created () {
@@ -24,4 +49,7 @@ export default {
 </script>
 
 <style scoped>
+.card {
+  margin: 50px 0;
+}
 </style>
